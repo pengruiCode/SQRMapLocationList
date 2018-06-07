263d902af25f839092b8ad5dcc5617cb4d838604
pengrui <pengrui@email.com>
2018-06-07 16:54:37 +0800

    Initial Commit

---
+++ SQRMapLocationList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SQRMapLocationList
+//
+//  Created by macMini on 2018/6/7.
+//  Copyright © 2018年 PR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SQRMapLocationList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SQRMapLocationList
+//
+//  Created by macMini on 2018/6/7.
+//  Copyright © 2018年 PR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
